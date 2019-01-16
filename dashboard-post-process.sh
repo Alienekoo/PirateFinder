c107aff51b634ec8813185600be02a61ee5e9679
ncta <mtooley@ncta.com>
2019-01-16 14:18:11 -0500

    Changed it to scan for p_malware to match what Joy v4.0.0 outputs

--- dashboard-post-process.sh
+++ dashboard-post-process.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 # usage ./post-process.sh <joy output results> <probability>
 # echo Post-processing $1 for results with probability greater than $2
-/PirateHunter/sleuth $1 --select "sa, da, sp, dp, p_piracy, bytes_out, num_pkts_out, time_start" --where "da~8.8.8.8, da~8.8.8.4, da~192.168.8.131, sa~192.168.8.131,da~224.0.0.*, da~172.*, da~255.*, da~239.*, dp~53, dp~443, dp~37, p_piracy > $2"
+/PirateHunter/sleuth $1 --select "sa, da, sp, dp, p_malware, bytes_out, num_pkts_out, time_start" --where "da~8.8.8.8,\
+ da~8.8.8.4, \
+ da~224.0.0.*, \
+ da~172.*, \
+ da~255.*, \
+ da~239.*, \
+ dp~53, \
+ dp~443,\
+ dp~37, \
+ p_piracy > $2"
 
