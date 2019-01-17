954b0a94747a895f81322dc2b6a66fd39e02777b
ncta <mtooley@ncta.com>
2019-01-17 10:16:32 -0500

    Changed to make it use a configuration file

--- output/probe.sh
+++ output/probe.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 # usage ./probe.sh <interface_name>
-../bin/joy \
-       bidir=1 \
-       dist=1 \
-       classify=1 \
-       output=auto \
-       count=10 \
-       verbosity=3 \
-       show_interfaces=1 \
-       username=root \
-       model="~/PirateFinder/classifiers/params.txt:~/PirateFinder/classifiers/params_bd.txt" \
-       interface=$1
+../bin/joy -x ../configuration/probe.conf
