a78f8910a93bab1bd01fc7699b81c56e792703b3
matt tooley <mtooley@ncta.com>
2019-04-03 13:26:27 -0400

    fixed path to clasifier

--- offline.sh
+++ offline.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-# usage ./offline.sh <pcap file> 
+# usage ./offline.sh <pcap file> <probability threshold> 
+echo "Step 1 - Analyze flows"
 ./bin/joy -x ./config/offline.conf $1 >tmp.gz
-./dashboard-post-process.sh tmp.gz 0 > tmp_results.txt
+echo "Step 2 - Extract flows of interest with Sleuth"
+./dashboard-post-process.sh tmp.gz $2 > tmp_results.txt
+echo "Step 3 - Post process and run through white list filter"
 python Dashboard.py -i tmp_results.txt -o N 
