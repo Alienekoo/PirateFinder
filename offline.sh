f21a7d196547e085a9d842eef6e3fa78a61c3885
matt tooley <mtooley@ncta.com>
2019-04-04 15:21:37 -0400

    added whitelist

--- offline.sh
+++ offline.sh
@@ -5,4 +5,4 @@ echo "Step 1 - Analyze flows"
 echo "Step 2 - Extract flows of interest with Sleuth"
 ./dashboard-post-process.sh tmp.gz $2 > tmp_results.txt
 echo "Step 3 - Post process and run through white list filter"
-python Dashboard.py -i tmp_results.txt -o N 
+python Dashboard.py -i tmp_results.txt -o N -w whitelist.txt 
